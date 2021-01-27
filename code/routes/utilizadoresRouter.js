57c1c200f692913ae9ba9e2cf7c9cd24437aab74
MartaMarquesSantos <55880824+MartaMarquesSantos@users.noreply.github.com>
2021-01-27 21:56:00 +0000

    adicionei login e registo

--- code/routes/utilizadoresRouter.js
+++ code/routes/utilizadoresRouter.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var utilizadoresModel=require('../Models/utilizadoresModel') 
-
-router.get('/', async function(req, res, next) {
-    let result = await utilizadoresModel.getAllUtilizadores();
-    res.status(result.status).send(result.data);
-});
+var utilizadoresModel = require('../Models/utilizadoresModel') 
 
 router.get('/:pos', async function(req, res, next) {
     let pos = req.params.pos;
-    let result = await utilizadoresModel.getUtilizador(pos);
+    let result = await utilizadoresModel.getUser(pos);
     res.status(result.status).send(result.data);
 });
 
-router.post('/login', async function(req, res, next) {
-    let result = await utilizadoresModel.login(req.body.username);
-    res.status(result.status).
-       send(result.data);
-  });
+router.get('/', async function(req, res, next) {
+    let obj = req.query;
+    let result = await utilizadoresModel.checkLogin(obj);
+    res.status(result.status).send(result.data);
+});
+
+router.post('/', async function(req, res, next) {
+    let user = req.body;
+    let result = await utilizadoresModel.createUser(user);
+    res.status(result.status).send(result.data);
+});
   
 module.exports = router;
